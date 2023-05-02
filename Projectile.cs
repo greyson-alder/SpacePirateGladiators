c496f07baedc040963bce4534409bcb5ecd5575e
Iain Sandison <iain.sandison.mousa@gmail.com>
2023-05-02 15:27:33 +0100

    add projectile lifetime property and logic

--- Projectile.cs
+++ Projectile.cs
@@ -11,6 +11,10 @@ public partial class Projectile : Area2D
 	private Element _element;
 	private double _damage;
 
+	private int _lifetime = 500;
+
+	private Area2D _main;
+
 	public void init(Vector2 direction, Element element, double damage) {
 		_direction = direction;
 		_element = element;
@@ -20,12 +24,19 @@ public partial class Projectile : Area2D
 	public override void _Ready()
 	{
 		GD.Print(this.ToString());
+		_main = GetNode<Area2D>("main");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		Position += Speed * _direction * (float)delta;
+		_lifetime -= 1;
+		GD.Print(_lifetime);
+		
+		if (_lifetime < 0) {
+			clearProjectile();
+		}
 		// GD.Print(Position.ToString());
 	}
 
@@ -38,8 +49,16 @@ public partial class Projectile : Area2D
 	}
 
 	private void OnNoLongerVisible() {
-		QueueFree();
+		clearProjectile();
 	}
+
+	private void OnLeavingMainBattleground(Area2D _main) {
+		clearProjectile();
+	}
+
+	private void clearProjectile() => QueueFree();
+
+
 
 
 }
