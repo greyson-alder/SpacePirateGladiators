ac8c5a534cfe7207307c176873fe6cb6d89ed4e0
Iain Sandison <iain.sandison.mousa@gmail.com>
2023-05-08 22:04:39 +0100

    add character sprite. breaks collision

--- Player.cs
+++ Player.cs
@@ -52,7 +52,7 @@ public partial class Player : CharacterBody2D
 		// 	y: Position.Y
 		// );
 		MoveAndSlide();
-		LookAt(GetGlobalMousePosition());
+		// LookAt(GetGlobalMousePosition());
 	}
 
 	public void handleMouseClick() {
